5a1a024d4fd0016e3e8ba8791e301515946373e1
Taaaaab <owen.whitsett@gmail.com>
2022-06-11 19:56:41 -0400

    Fixed layout, setting up JavaScript for books

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const container = document.querySelector('#container');
+const bookCard = document.querySelector('#bookCard');
+const card1 = document.querySelector('#card1');
+const card2 = document.querySelector('#card2');
+const card3 = document.querySelector('#card3');
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -8,6 +15,24 @@ function Book(title, author, pages, read) {
     }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'have read');
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'have read');
+const book2 = new Book('Goosebumps XD', 'J.R.R.', '215 pages', 'have not read');
+const book3 = new Book('LOTR', 'J.R.', '445 pages', 'have not read');
 
-console.log(theHobbit.info());
+const ourLibrary = {
+    book1,
+    book2,
+    book3
+};
+
+function addBookToLibrary(books) {
+    for (let book in books) {
+        myLibrary.push(book);
+        card1.textContent = `${myLibrary[0]}`;
+        card2.textContent = `${myLibrary[1]}`;
+        card3.textContent = `${myLibrary[2]}`;
+    };
+};
+
+addBookToLibrary(ourLibrary);
+console.log(myLibrary);
