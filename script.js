12d0aa69270129a35879cd1f8d7b6c28d510fd14
Taaaaab <owen.whitsett@gmail.com>
2022-07-03 14:29:56 -0400

    Fix read radio button

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const addBook = (e)=> {
     const book = new Book(document.getElementById('title').value, 
     document.getElementById('author').value, 
     document.getElementById('pages').value, 
-    document.getElementById('yes').value)
+    document.querySelector('input[name="read"]:checked').value);
     
     addBookToLibrary(book);
     console.log(myLibrary);
@@ -71,6 +71,7 @@ const addBook = (e)=> {
   deleteBook.addEventListener('click', () => {
       cardBox.classList.add('card-box-delete');
     });
+
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
